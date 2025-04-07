7350b6d0355de04bca1d86fd64f4e0d00d7864b4
Vamshidhar Reddy <91316387+Vamc-44@users.noreply.github.com>
2025-04-06 20:25:45 -0500

    Update Sleep_data_CSV.py

--- Sleep_data_CSV.py
+++ Sleep_data_CSV.py
@@ -1,15 +1,18 @@
-from XML_to_CSV_Script import parse_xml_to_dataframe
+# This script reads an XML file containing sleep data, parses it, and converts it into a CSV file.
+from XML_to_CSV_Script import parse_sleep_data
 import pandas as pd
 
 #Read the XML file into a pandas DataFrame
 xml_file = 'export/apple_health_export/export.xml'
-df = parse_xml_to_dataframe(xml_file)
+df = parse_sleep_data(xml_file)
 print(df.head()) # Display the first few rows of the DataFrame  
 
 #Converting the DataFrame to CSV  
 df.to_csv('sleep_data.csv', index=False)
 print('XML data successfully converted to CSV.')
-print(df.head())  # Display the first few rows of the DataFrame 
+print(df.head())  # Display the first few rows of the DataFrame
+
+
 
 
 
